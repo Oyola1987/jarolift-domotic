a5651d201837fa7535d5c300e3ccc0048bbc3b32
Jose Oyola <info@josoy.com>
2020-03-03 20:43:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jarolift-domotic
---
+++ src/main/java/com/jarolift/domotic/controllers/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.jarolift.domotic.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Called");
+        return "Greetings from Spring Boot!";
+    }
+}
