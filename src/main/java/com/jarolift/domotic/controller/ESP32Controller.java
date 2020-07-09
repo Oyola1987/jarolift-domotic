76c213a75c8b3f828ebb2a4657f953e79e0d2d0e
Jose Oyola <info@josoy.com>
2020-07-09 16:08:12 +0200

    refactor in esp32controller

--- src/main/java/com/jarolift/domotic/controller/ESP32Controller.java
+++ src/main/java/com/jarolift/domotic/controller/ESP32Controller.java
@@ -2,8 +2,6 @@ package com.jarolift.domotic.controller;
 
 import com.jarolift.domotic.service.ESP32Versions;
 import com.jarolift.domotic.service.EmailService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -13,16 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @RestController
 public class ESP32Controller {
-    private static final String NO_FILES = "No 'bin' files";
-    private static Logger logger = LogManager.getLogger(ESP32Controller.class);
     private EmailService emailService;
     private ESP32Versions esp32Versions;
 
@@ -32,28 +22,17 @@ public class ESP32Controller {
         this.esp32Versions = esp32Versions;
     }
 
-    @GetMapping(value = "/api/esp32/version/lastest", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/api/esp32/version", produces = MediaType.TEXT_PLAIN_VALUE)
     public String getLastVersion(@RequestHeader(value = "User-Agent") String userAgent) {
-        File file = esp32Versions.getLastestVersion();
-        String version = file != null ? file.getName() : NO_FILES;
-        logger.info("[USER AGENT]: " + userAgent);
-        logger.info("[LAST VERSION]: " + version);
-        return file == null ? null : version;
+        return esp32Versions.getLastVersion(userAgent);
     }
 
-    @GetMapping(value = "/api/esp32/update/lastest", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<Resource> updateOTA(@RequestHeader(value = "User-Agent") String userAgent) throws IOException {
-        File file = esp32Versions.getLastestVersion();
-        String version = file != null ? file.getName() : NO_FILES;
-        logger.info("[USER AGENT]: " + userAgent);
-        logger.info("[LAST VERSION]: " + version);
-        if(file == null)
-            return null;
+    @GetMapping(value = "/api/esp32/update", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<Resource> updateOTA(@RequestHeader(value = "User-Agent") String userAgent) {
+        ByteArrayResource resource = esp32Versions.getLastFile(userAgent);
 
-        Path path = Paths.get(file.getAbsolutePath());
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-        return ResponseEntity.ok()
-                .contentLength(file.length())
+        return resource == null ? null : ResponseEntity.ok()
+                .contentLength(resource.contentLength())
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(resource);
     }
--- src/main/java/com/jarolift/domotic/service/ESP32Versions.java
+++ src/main/java/com/jarolift/domotic/service/ESP32Versions.java
@@ -1,27 +1,69 @@
 package com.jarolift.domotic.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 
 @Controller
 public class ESP32Versions {
+    private static final String NO_FILES = "No 'bin' files";
     private final static String FOLDER_PATH = "./bin_esp32";
+    private static Logger logger = LogManager.getLogger(ESP32Versions.class);
     File dir = new File(FOLDER_PATH);
 
     public ESP32Versions() {
-        if (!dir.exists()) {
-            dir.mkdir();
+        ensureFolder();
+    }
+
+    public ByteArrayResource getLastFile(String userAgent) {
+        File file = getLatestFile();
+        String version = file != null ? file.getName() : NO_FILES;
+        logger.info("[USER AGENT]: " + userAgent);
+        logger.info("[LAST VERSION]: " + version);
+
+        if(file == null) {
+            return null;
+        } else {
+            ByteArrayResource resource = null;
+            try {
+                Path path = Paths.get(file.getAbsolutePath());
+                resource = new ByteArrayResource(Files.readAllBytes(path));
+            } catch (Exception e) {
+                logger.error("Error user agent: " + userAgent);
+                logger.error("Error message: " + e.getMessage());
+            }
+            return resource;
         }
     }
 
-    public File getLastestVersion() {
+    public String getLastVersion(String userAgent) {
+        File file = getLatestFile();
+        String version = file != null ? file.getName() : NO_FILES;
+        logger.info("[USER AGENT]: " + userAgent);
+        logger.info("[LAST VERSION]: " + version);
+        return file == null ? null : version;
+    }
+
+    private File getLatestFile() {
+        ensureFolder();
         File [] binFiles = dir.listFiles((dir, name) -> name.endsWith(".bin"));
 
         Arrays.sort(binFiles, Collections.reverseOrder());
 
         return binFiles.length > 0 ? binFiles[0] : null;
+    }
+
+    private void ensureFolder() {
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
     }
 }
