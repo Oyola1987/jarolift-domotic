7a0beea351008e49d8cd458bd89a55201128a5d9
Jose Oyola <info@josoy.com>
2020-10-27 18:24:22 +0100

    change method name

--- src/main/java/com/jarolift/domotic/controller/KeepAliveController.java
+++ src/main/java/com/jarolift/domotic/controller/KeepAliveController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KeepAliveController {
     @GetMapping(value = "/api/keepalive")
-    public ResponseEntity pushButton() {
+    public ResponseEntity keepAlive() {
         return ResponseEntity.ok().body("KEEP ALIVE OK");
     }
 }
